913d0eb4fe54a882377c4ae64a1c27b732557420
MrAlcoholicFox <cthulhu345@gmail.com>
2023-07-10 11:47:13 +1200

    Added support for textures, you'll have to tweak a bit to get it working as sdl is weird, can probs fix soon. Converts images to array of rgb values to make stuff faster

--- raycaster.lisp
+++ raycaster.lisp
@@ -1,5 +1,8 @@
 ;;;; Le pisscaster
 
+;;;; TODO
+; probably make a check that converts the texture things
+
 ;;Poss check translate shit etc
 
 ;;; lib
@@ -10,11 +13,11 @@
 ;;; Some general parameters
 (defparameter *mapwidth* 24)
 (defparameter *mapheight* 24)
-(defparameter *screenwidth* 640)
-(defparameter *screenheight* 480)
+(defparameter *screenwidth* 960)
+(defparameter *screenheight* 720)
 (defparameter *half-height* (/ *screenheight* 2))
 (defparameter *half-width* (/ *screenwidth* 2))
-(defparameter *fps* 30)
+(defparameter *fps* 60)
 (defparameter *ang-increment* nil) ; Set later as it relies on player fov
 (defparameter *ray-precision* 64)
 (defparameter *scale* 1) ;probably tweak a bit
@@ -30,16 +33,26 @@
 
 
 (defparameter *worldmap*
+  ;; #2A((1 1 1 1 1 1 1 1 1 1 1 1)
+  ;;     (1 0 0 0 0 0 0 0 0 0 0 1)
+  ;;     (1 0 1 1 1 1 0 1 1 1 0 1)
+  ;;     (1 0 1 0 0 0 0 0 0 1 0 1)
+  ;;     (1 0 1 0 1 1 1 1 0 1 0 1)
+  ;;     (1 0 1 0 1 0 0 0 0 1 0 1)
+  ;;     (1 0 1 0 1 1 1 1 0 1 0 1)
+  ;;     (1 0 0 0 0 0 0 0 0 0 0 1)
+  ;;     (1 1 1 1 1 1 1 1 1 1 1 1)))
+
   #2A((1 1 1 1 1 1 1 1 1 1)
       (1 0 0 0 0 0 0 0 0 1)
       (1 0 0 0 0 0 0 0 0 1)
-      (1 0 0 1 1 0 1 0 0 1)
+      (1 0 0 0 0 0 1 0 0 1)
       (1 0 0 1 0 0 1 0 0 1)
       (1 0 0 1 0 0 1 0 0 1)
       (1 0 0 1 0 1 1 0 0 1)
+      (1 0 0 1 0 0 0 0 0 1)
       (1 0 0 0 0 0 0 0 0 1)
-      (1 0 0 0 0 0 0 0 0 1)
-      (1 1 1 1 1 1 1 1 1 1)))
+      (1 1 1 1 1 1 1 1 1 1))) 
 
 ;;; Textures
 (defstruct texture width height bitmap colors) ; probably fix the color thing later lmao, can probs convert to list
@@ -55,14 +68,29 @@
 				   (0 0 0 1 0 0 0 1)
 				   (1 1 1 1 1 1 1 1)
 				   (0 1 0 0 0 1 0 0))
-		       :colors (list (sdl:color :r 255 :g 241 :b 232) (sdl:color :r 194 :g 195 :b 199))))
+		       :colors (list (sdl:color :r 255 :g 241 :b 232)
+				     (sdl:color :r 194 :g 195 :b 199) (sdl:color :r 50 :g 40 :b 20))))
 
-;(defparameter *brick2* (make-texture
-;			:width 16
-;			:height 16))
-			
+(defparameter *brick2* (make-texture
+			:width 16
+			:height 16
+			:bitmap "img/brickedup.png"
+			:colors nil))
 
-(defparameter *textures* (list *brick*))
+
+(defparameter *textures* (list *brick2* *brick*))
+
+(defun texture-to-array (tex)
+  (let* ((img (texture-bitmap tex))
+	 (height (texture-height tex))
+	 (width (texture-width tex))
+	 (tex-image (sdl:load-image img))
+	 (temp-arr (make-array `(,width ,height))))
+    (dotimes (row height)
+      (dotimes (col width)
+	(setf (aref temp-arr row col) (sdl:read-pixel-* row col :surface tex-image))))
+    (setf (texture-bitmap tex) temp-arr)))
+	
 
 ;;; The player struct and global player variable (imagine using classes lmao)
 (defstruct player posx posy angle fov halffov movement rotation)
@@ -117,11 +145,20 @@
 (defun drawtexture (x wallheight texture-posx txture)
   (let ((y-incrementer (/ (* wallheight 2) (texture-height txture)))
 	(y (- *proj-hlf-height* wallheight)))
-    (dotimes (i (texture-height txture))
-      (let ((color (nth (aref (texture-bitmap txture) i texture-posx) (texture-colors txture))))
-	(sdl:draw-line (sdl:point :x x :y y)
-		       (sdl:point :x x :y (+ y (+ y-incrementer 0.5))) :color color :surface *surface*)
-	(incf y y-incrementer)))))
+
+    (when (not (null (texture-colors txture)))
+      (dotimes (i (texture-height txture)) ; check if the texture supplied is an array or image
+	(let ((color (nth (aref (texture-bitmap txture) i texture-posx) (texture-colors txture))))
+	  (sdl:draw-line (sdl:point :x x :y y)
+			 (sdl:point :x x :y (+ y (+ y-incrementer 0.5))) :color color :surface *surface*)
+	  (incf y y-incrementer))))
+
+    (when (null (texture-colors txture))
+      (dotimes (i (texture-height txture))
+	(let ((color (aref (texture-bitmap txture) texture-posx i)))
+	  (sdl:draw-line (sdl:point :x x :y y)
+			 (sdl:point :x x :y (+ y (+ y-incrementer 0.5))) :color color :surface *surface*)
+	  (incf y y-incrementer))))))
 
 (defun raycasting ()
   (let ((rayangle (- (player-angle *player*) (player-halffov *player*))))
@@ -136,9 +173,7 @@
 	(loop do
 	  (incf x raycos)
 	  (incf y raysin)
-	  (if (>= y 10) (setf y 9)) ;stupid floating shit
-	  (if (>= x 10) (setf x 9)) ;stupid floating shit
-	 ; (format t "X: ~A Y: ~A RAYCOS: ~A RAYSIN: ~A~%" x y raycos raysin)
+	  ;(format t "X: ~A Y: ~A RAYCOS: ~A RAYSIN: ~A~%" x y raycos raysin)
 	  (setf wall (aref *worldmap* (floor y) (floor x)))
 	      while (= wall 0))
 
@@ -156,22 +191,22 @@
 	  #| (sdl:draw-line (sdl:point :x raycount :y (- *proj-hlf-height* wallheight))
 			 (sdl:point :x raycount :y (+ *proj-hlf-height* wallheight)) :color sdl:*red*
 	  :surface *surface*) |#
-
+	  
 	  (drawtexture raycount wallheight texture-posx txture)
 
 	  (sdl:draw-line (sdl:point :x raycount :y (+ *proj-hlf-height* wallheight))
 			 (sdl:point :x raycount :y *proj-height*) :color (sdl:color :r 95 :g 87 :b 79)
 								  :surface *surface*)
-	  
 	  (incf rayangle *ang-increment*))))))       
 
-(defun test1 ()
+(defun pissharder ()
   (sdl:with-init ()
     (sdl:window *screenwidth* *screenheight* :title-caption "pissraycast" :sw t)
     (setf (sdl:frame-rate) *fps*)
     (setf *surface* (sdl:create-surface *proj-width* *proj-height*))
-    (setf *player* (make-player :posx 2 :posy 2 :angle 90 :fov 60 :halffov 30 :movement 0.3 :rotation 5.0))
+    (setf *player* (make-player :posx 2 :posy 2 :angle 90 :fov 60 :halffov 30 :movement 0.2 :rotation 3.0))
     (setf *ang-increment* (/ (player-fov *player*) *proj-width*)) ; changed to use scale stuff
+    (texture-to-array *brick2*)
     (sdl:with-events ()
       (:quit-event () t)
       (:idle ()   	     
@@ -179,8 +214,8 @@
 	     (keyhandler)
 	     (raycasting)
 	     (sdl:draw-surface (sdl:zoom-surface *scale* *scale* :surface *surface*))
-	     (sdl:draw-surface-at-* (sdl:zoom-surface 3.5 3 :surface (sdl-image:load-image "img/handspray.png" :color-key-at #(0 0))) (- *screenwidth* 350) (- *screenheight* 300))
+	    ; (sdl:draw-surface-at-* (sdl:zoom-surface 3.5 3 :surface (sdl-image:load-image "img/handspray.png" :color-key-at #(0 0))) (- *screenwidth* 350) (- *screenheight* 300))
 	     (sdl:update-display)))))
 
 
-(test1)
+(pissharder)
